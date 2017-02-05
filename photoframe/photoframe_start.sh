70bb895bd2b280e57a7f114ed59b9ffece7f7108
UWAGUCHI Shinji <s.uwaguchi@gmail.com>
2017-02-06 00:04:54 +0900

    オプション変更

--- photoframe/photoframe_start.sh
+++ photoframe/photoframe_start.sh
@@ -4,4 +4,4 @@
 TARGET=/home/uwaguchi/raspberrypi/photoframe/filelist.txt
 
 # feh起動
-DISPLAY=:0.0 feh -D 10 -z -Y -q -F -f ${TARGET}
+DISPLAY=:0.0 feh -D 10 -z -Y -q -F --draw-filename --draw-tinted -f ${TARGET}
