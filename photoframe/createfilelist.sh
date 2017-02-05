0e7b9f2483ee5632f5a555c751ec26bb9f02267d
UWAGUCHI Shinji <s.uwaguchi@gmail.com>
2017-02-05 23:30:05 +0900

    cron対応

--- photoframe/createfilelist.sh
+++ photoframe/createfilelist.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-find /mnt/share/photo/2006 -type f > filelist.work
-find /mnt/share/photo/2007 -type f >> filelist.work
-find /mnt/share/photo/2008 -type f >> filelist.work
-find /mnt/share/photo/2009 -type f >> filelist.work
-find /mnt/share/photo/201* -type f >> filelist.work
-find /mnt/share/Eye-Fi -type f >> filelist.work
-perl -lne 'print if /JPG$|jpg$/' filelist.work > filelist.txt
-rm -f filelist.work
+FILELISTPATH=/home/uwaguchi/raspberrypi/photoframe
+
+find /mnt/share/photo/2006 -type f > ${FILELISTPATH}/filelist.work
+find /mnt/share/photo/2007 -type f >> ${FILELISTPATH}/filelist.work
+find /mnt/share/photo/2008 -type f >> ${FILELISTPATH}/filelist.work
+find /mnt/share/photo/2009 -type f >> ${FILELISTPATH}/filelist.work
+find /mnt/share/photo/201* -type f >> ${FILELISTPATH}/filelist.work
+find /mnt/share/Eye-Fi -type f >> ${FILELISTPATH}/filelist.work
+perl -lne 'print if /JPG$|jpg$/' ${FILELISTPATH}/filelist.work > ${FILELISTPATH}/filelist.txt
+rm -f ${FILELISTPATH}/filelist.work
