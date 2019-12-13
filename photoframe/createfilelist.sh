0cf82219927b17fe38720febb81e2fc37bb58da6
Shinji Uwaguchi <s.uwaguchi@gmail.com>
2019-12-13 23:48:24 +0900

    写真パス修正

--- photoframe/createfilelist.sh
+++ photoframe/createfilelist.sh
@@ -2,11 +2,11 @@
 
 FILELISTPATH=/home/uwaguchi/raspberrypi/photoframe
 
-find /mnt/share/photo/2006 -type f > ${FILELISTPATH}/filelist.work
-find /mnt/share/photo/2007 -type f >> ${FILELISTPATH}/filelist.work
-find /mnt/share/photo/2008 -type f >> ${FILELISTPATH}/filelist.work
-find /mnt/share/photo/2009 -type f >> ${FILELISTPATH}/filelist.work
-find /mnt/share/photo/201* -type f >> ${FILELISTPATH}/filelist.work
-find /mnt/share/Eye-Fi -type f >> ${FILELISTPATH}/filelist.work
+find /mnt/Public/photo/2006 -type d -name  '.*' -prune -or -not -name '.*' > ${FILELISTPATH}/filelist.work
+find /mnt/Public/photo/2007 -type d -name  '.*' -prune -or -not -name '.*' >> ${FILELISTPATH}/filelist.work
+find /mnt/Public/photo/2008 -type d -name  '.*' -prune -or -not -name '.*' >> ${FILELISTPATH}/filelist.work
+find /mnt/Public/photo/2009 -type d -name  '.*' -prune -or -not -name '.*' >> ${FILELISTPATH}/filelist.work
+find /mnt/Public/photo/201* -type d -name  '.*' -prune -or -not -name '.*' >> ${FILELISTPATH}/filelist.work
+find /mnt/Public/Eye-Fi -type d -name  '.*' -prune -or -not -name '.*' >> ${FILELISTPATH}/filelist.work
 perl -lne 'print if /JPG$|jpg$/' ${FILELISTPATH}/filelist.work > ${FILELISTPATH}/filelist.txt
 rm -f ${FILELISTPATH}/filelist.work
