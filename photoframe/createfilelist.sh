48c9c5913aa5b570f12f9b710f68b77a618b47a4
Shinji Uwaguchi <s.uwaguchi@gmail.com>
2019-12-14 21:56:14 +0900

    パス変更

--- photoframe/createfilelist.sh
+++ photoframe/createfilelist.sh
@@ -2,11 +2,11 @@
 
 FILELISTPATH=/home/uwaguchi/raspberrypi/photoframe
 
-find /mnt/Public/photo/2006 -type d -name  '.*' -prune -or -not -name '.*' > ${FILELISTPATH}/filelist.work
-find /mnt/Public/photo/2007 -type d -name  '.*' -prune -or -not -name '.*' >> ${FILELISTPATH}/filelist.work
-find /mnt/Public/photo/2008 -type d -name  '.*' -prune -or -not -name '.*' >> ${FILELISTPATH}/filelist.work
-find /mnt/Public/photo/2009 -type d -name  '.*' -prune -or -not -name '.*' >> ${FILELISTPATH}/filelist.work
-find /mnt/Public/photo/201* -type d -name  '.*' -prune -or -not -name '.*' >> ${FILELISTPATH}/filelist.work
-find /mnt/Public/Eye-Fi -type d -name  '.*' -prune -or -not -name '.*' >> ${FILELISTPATH}/filelist.work
+find /mnt/Public/share/photo/2006 -type d -name  '.*' -prune -or -not -name '.*' > ${FILELISTPATH}/filelist.work
+find /mnt/Public/share/photo/2007 -type d -name  '.*' -prune -or -not -name '.*' >> ${FILELISTPATH}/filelist.work
+find /mnt/Public/share/photo/2008 -type d -name  '.*' -prune -or -not -name '.*' >> ${FILELISTPATH}/filelist.work
+find /mnt/Public/share/photo/2009 -type d -name  '.*' -prune -or -not -name '.*' >> ${FILELISTPATH}/filelist.work
+find /mnt/Public/share/photo/201* -type d -name  '.*' -prune -or -not -name '.*' >> ${FILELISTPATH}/filelist.work
+find /mnt/Public/share/Eye-Fi -type d -name  '.*' -prune -or -not -name '.*' >> ${FILELISTPATH}/filelist.work
 perl -lne 'print if /JPG$|jpg$/' ${FILELISTPATH}/filelist.work > ${FILELISTPATH}/filelist.txt
 rm -f ${FILELISTPATH}/filelist.work
